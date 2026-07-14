d5e9a1630c2ba727128816315ff1ed5819898436
Sujith George <>
2026-07-14 11:15:56 +0530

    navigation issue

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,11 @@ import 'package:flutter/material.dart';
 import 'app_environment.dart';
 import 'app_theme.dart';
 import 'features/auth/login_screen.dart';
+import 'navigation/browser_history_sync.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  configureBrowserHistorySync();
   await AppEnvironment.load();
   runApp(const MpcPharmaApp());
 }
@@ -13,12 +15,16 @@ Future<void> main() async {
 class MpcPharmaApp extends StatelessWidget {
   const MpcPharmaApp({super.key});
 
+  static final NavigatorObserver _browserHistoryObserver =
+      createBrowserHistorySyncObserver();
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'MPC Pharma',
       debugShowCheckedModeBanner: false,
       theme: AppTheme.theme,
+      navigatorObservers: [_browserHistoryObserver],
       home: const LoginScreen(),
     );
   }
---
+++ lib/navigation/browser_history_sync_stub.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/widgets.dart';
+
+void configureBrowserHistorySync() {}
+
+NavigatorObserver createBrowserHistorySyncObserver() {
+  return NavigatorObserver();
+}
